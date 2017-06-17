13bee030352cbb3f8c38308344f2593109409f75
awsengineer <mehdi@awsengineer.com>
2017-06-17 20:51:01 +1000

    CF Signed URL usign Custom poliy was added.

--- src/main/java/CloudFront_SignedURL_CustomPolicy.java
+++ src/main/java/CloudFront_SignedURL_CustomPolicy.java
@@ -10,45 +10,29 @@ import java.io.FileInputStream;
 import java.security.Security;
 
 public class CloudFront_SignedURL_CustomPolicy {
+    public static void main(String[] args) {
+        try {
+            String privateKeyFilePath = "/Users/mssalehi/keys/pk-APKAIMN2TXDRB53WEPFQ.der";
 
-        public static void main(String[] args) {
-            try {
-                String privateKeyFilePath = "/Users/mssalehi/keys/pk-APKAIMN2TXDRB53WEPFQ.der";
+            Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
+            String distributionDomain = "d1qkkjuhp2kr5s.cloudfront.net";
+            String s3ObjectKey = "aws.png";
+            String policyResourcePath = "https://" + distributionDomain + "/" + s3ObjectKey;
 
-                Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
-                String distributionDomain = "d1qkkjuhp2kr5s.cloudfront.net";
-                String s3ObjectKey = "a.jpg";
-                String policyResourcePath = "https://" + distributionDomain + "/" + s3ObjectKey;
+            byte[] derPrivateKey = IOUtils.toByteArray(new FileInputStream(privateKeyFilePath));
+            System.out.println(policyResourcePath);
+            //String keyPairId = "AKIAIG66DBN5GJR5PGZA"; APKAIMN2TXDRB53WEPFQ
+            String keyPairId = "APKAIMN2TXDRB53WEPFQ";
+            java.util.Date expiresOn = ServiceUtils.parseIso8601Date("2017-02-14T22:20:00.000Z");
+            System.out.println(expiresOn);
 
-                byte[] derPrivateKey = IOUtils.toByteArray(new FileInputStream(privateKeyFilePath));
-                System.out.println(policyResourcePath);
-                //String keyPairId = "AKIAIG66DBN5GJR5PGZA"; APKAIMN2TXDRB53WEPFQ
-                String keyPairId = "APKAIMN2TXDRB53WEPFQ";
-                java.util.Date expiresOn = ServiceUtils.parseIso8601Date("2017-02-14T22:20:00.000Z");
-                System.out.println(expiresOn);
 
-                String signedUrlCanned =  CloudFrontService.signUrlCanned(
-                        //"https://" + distributionDomain + "/" + s3ObjectKey,
-                        "https://d1qkkjuhp2kr5s.cloudfront.net/a.jpg",
-                        //policyResourcePath,
-                        keyPairId,
-                        derPrivateKey,
-                        expiresOn
-                );
-                System.out.println("signedUrlCanned:");
-                System.out.println(signedUrlCanned);
-                System.out.println("End of Canned policy.");
-            /*
             // Build a policy document to define custom restrictions for a signed URL.
             String policy = CloudFrontService.buildPolicyForSignedUrl(
-// Resource path (optional, can include '*' and '?' wildcards)
                     policyResourcePath,
-// DateLessThan
-                    ServiceUtils.parseIso8601Date("2017-11-14T22:20:00.000Z"),
-
-                    "52.52.10.0/24",
-                    // DateGreaterThan (optional)
-                    ServiceUtils.parseIso8601Date("2017-10-16T06:31:56.000Z")
+                    ServiceUtils.parseIso8601Date("2017-09-14T22:20:00.000Z"), // Date must be less than this
+                    "54.240.193.0/24",
+                    ServiceUtils.parseIso8601Date("2017-01-16T06:31:56.000Z") // Date must be greater than (optional)
             );
 
             // Generate a signed URL using a custom policy document.
@@ -61,15 +45,14 @@ public class CloudFront_SignedURL_CustomPolicy {
             System.out.println("policy:");
             System.out.println(policy);
             System.out.println("signedUrl:");
-            System.out.println(signedUrl);  */
-            }
-            catch (java.io.IOException e){
-                System.out.println(e.toString());
-            }
-            catch (org.jets3t.service.CloudFrontServiceException e){
-                System.out.println(e.toString());
-            }
+            System.out.println(signedUrl);
+
+        } catch (java.io.IOException e) {
+            System.out.println(e.toString());
+        } catch (org.jets3t.service.CloudFrontServiceException e) {
+            System.out.println(e.toString());
         }
     }
+}
 
 
